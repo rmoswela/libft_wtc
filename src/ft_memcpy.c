4b6bb1cba2359f320161707f3d68ee35cf653126
reu_the_joker <reu_the_joker@localhost.localdomain>
2016-11-21 07:25:23 +0200

    fixed the pointer arithmetic that gave wrong results

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -1,5 +1,7 @@
 #include "libft.h"
 
+/*function to copy first n bytes from memory area src
+ * to memory area dest without overlapping the memory areas*/
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned char	*cDest;
