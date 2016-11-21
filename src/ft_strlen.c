c911d6b56f999acef32b3335cf5c8f93d0ca6950
Reuben MOSWELA <rmoswela@e5r6p11.jnb.42.fr>
2016-11-21 13:47:14 +0200

    corrected library path

--- src/ft_strlen.c
+++ src/ft_strlen.c
@@ -6,11 +6,11 @@
 /*   By: rmoswela <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/21 13:15:37 by rmoswela          #+#    #+#             */
-/*   Updated: 2016/11/21 13:25:28 by rmoswela         ###   ########.fr       */
+/*   Updated: 2016/11/21 13:41:15 by rmoswela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../libft.h"
 
 /*function to compute the length of string str*/
 int			ft_strlen(const char *str)
