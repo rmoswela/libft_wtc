50af3a854f6458d9cb0e52e3b150d947644c4472
reu_the_joker <reu_the_joker@localhost.localdomain>
2016-11-21 05:16:06 +0200

    function to copy n bytes from memory area src to dest without overlap

---
+++ ft_memcpy.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*cDest;
+	unsigned char	*cSrc;
+	size_t		i;
+
+	i = 0;
+	cDest = (unsigned char *) dest;
+	cSrc = (unsigned char *) src;
+	while (i < n)
+	{
+		if (cDest == cSrc)
+			ft_memmove(cDest, cSrc, i);
+		else
+			*cDest++ = *cSrc++;
+		i++;
+	}
+	*cDest = '\0';
+	return (cDest);
+}
